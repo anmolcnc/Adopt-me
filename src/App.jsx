a4e40859d22c592a5f5e43561f6e54f7ad873e0e
anmol <anmol@curvesncolors.ch>
2024-05-08 15:43:50 +0545

    relocate searchparms.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 // add to the top
 import React from "react";
 import { createRoot } from "react-dom/client";
-import SearchParams from "../SearchParams";
+import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
