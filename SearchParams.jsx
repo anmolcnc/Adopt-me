592114fff61602acce977a407d9eb88cc76f8132
anmol <anmol@curvesncolors.ch>
2024-05-08 15:42:27 +0545

    first commit

---
+++ SearchParams.jsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
+const breeds = [];
+
+const SearchParams = () => {
+  const [animal, setAnimal] = useState("");
+  const [location, setLocation] = useState("");
+  const [breed, setBreed] = useState("");
+  return (
+    <div className="search-params">
+      <form>
+        <label htmlFor="location">
+          Location
+          <input
+            id="location"
+            value={location}
+            placeholder="Location"
+            onChange={(e) => setLocation(e.target.value)}
+          />
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={(e) => {
+              setAnimal(e.target.value);
+              setBreed("");
+            }}
+          >
+            <option />
+            {ANIMALS.map((animal) => (
+              <option key={animal}>{animal}</option>
+            ))}
+          </select>
+        </label>
+        <label htmlFor="breed">
+          Breed
+          <select
+            id="breed"
+            value={breed}
+            onChange={(e) => setbreed(e.target.value)}
+            disabled={breeds.length === 0}
+          >
+            <option />
+            {breeds.map((breed) => (
+              <option key={breed}>{breed}</option>
+            ))}
+          </select>
+        </label>
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default SearchParams;
