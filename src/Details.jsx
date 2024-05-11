baa45ebff4a3de14aa93a90ae11e0cfc6ca1c677
Anmol <nmol.crestha@gmail.com>
2024-05-11 15:43:08 +0545

    added a modal using portal

--- src/Details.jsx
+++ src/Details.jsx
@@ -3,8 +3,11 @@ import { useQuery } from "@tanstack/react-query";
 import fetchPet from "./fetchPet";
 import Carousel from "./Carousel";
 import ErrorBoundary from "./ErrorBoundary";
+import Modal from "./Modal";
+import { useState } from "react";
 
 const Details = () => {
+  const [showModal, setShowModal] = useState(false);
   const { id } = useParams();
   const results = useQuery(["details", id], fetchPet);
 
@@ -24,8 +27,19 @@ const Details = () => {
       <div>
         <h1>{pet.name}</h1>
         <h2>{`${pet.animal} — ${pet.breed} — ${pet.city}, ${pet.state}`}</h2>
-        <button>Adopt {pet.name}</button>
+        <button onClick={() => setShowModal(true)}>Adopt {pet.name}</button>
         <p>{pet.description}</p>
+        {showModal && (
+          <Modal>
+            <div>
+              <h1>Would you like to adopt {pet.name}?</h1>
+              <div className="buttons">
+                <button>Yes</button>
+                <button onClick={() => setShowModal(false)}>No</button>
+              </div>
+            </div>
+          </Modal>
+        )}
       </div>
     </div>
   );
@@ -34,7 +48,7 @@ const Details = () => {
 const DetailsErrorBoundry = (props) => {
   return (
     <ErrorBoundary>
-      <Details {...props  }/>
+      <Details {...props} />
     </ErrorBoundary>
   );
 };
